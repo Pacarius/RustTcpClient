50784667c97dd90c266f56bd66aa6ce75132eb8b
Pacarius <pcpgsk@gmail.com>
2024-01-19 09:45:33 +0800

    YepCock

--- src/client.rs
+++ src/client.rs
@@ -2,51 +2,59 @@
 #![allow(unused_imports)]
 
 use std::env;
+use std::io::{self, Error, Read, Write};
+use std::net::IpAddr;
+use std::net::TcpListener;
+use std::net::TcpStream;
 use std::process;
 use std::thread;
-use std::io::{self, Read, Write, Error};
-use std::net::TcpStream;
-use std::net::TcpListener;
 
 type Port = u16;
 
-pub fn makeClient() {
-	let host = "127.0.0.1";
-	let port = "40269";
-	let mut stream = TcpStream::connect(
-		(host.as_str(), port)
-	).unwrap_or_else(|error|
-		program.print_fail(error.to_string())
-	);
-	let mut input_stream = stream.try_clone().unwrap();
+pub struct clientInfo {
+    host: IpAddr,
+    port: u16,
+}
 
-	let handler = thread::spawn(move || {
-		let mut client_buffer = [0u8; 1024];
+impl clientInfo {
+    fn makeClient(&self) {
+        let mut stream =
+            TcpStream::connect((self.host, self.port)).unwrap_or_else(|error| std::process::exit(0));
+        let mut input_stream = stream.try_clone().unwrap();
 
-		loop {
-			match input_stream.read(&mut client_buffer) {
-				Ok(n) => {
-					if n == 0 {
-						program.exit(0);
-					}
-					else
-					{
-						io::stdout().write(&client_buffer).unwrap();
-						io::stdout().flush().unwrap();
-					}
-				},
-				Err(error) => program.print_fail(error.to_string()),
-			}
-		}
-	});
+        let handler = thread::spawn(move || {
+            let mut client_buffer = [0u8; 1024];
 
-	let output_stream = &mut stream;
-	let mut user_buffer = String::new();
+            loop {
+                match input_stream.read(&mut client_buffer) {
+                    Ok(n) => {
+                        if n == 0 {
+                            std::process::exit(0);
+                        } else {
+                            io::stdout().write(&client_buffer).unwrap();
+                            io::stdout().flush().unwrap();
+                        }
+                    }
+                    Err(error) => std::process::exit(-1),
+                }
+            }
+        });
 
-	loop {
-		io::stdin().read_line(&mut user_buffer).unwrap();
+        let output_stream = &mut stream;
+        let mut user_buffer = String::new();
 
-		output_stream.write(user_buffer.as_bytes()).unwrap();
-		output_stream.flush().unwrap();
-	}
+        loop {
+            io::stdin().read_line(&mut user_buffer).unwrap();
+
+            Write::write_all(output_stream, user_buffer.as_bytes()).unwrap();
+            output_stream.flush().unwrap();
+        }
+    }
+}
+fn main(){
+    let client = clientInfo{
+        host:"127.0.0.1".parse().expect("Fuck you."),
+        port:42069
+    };
+    client.makeClient();
 }
